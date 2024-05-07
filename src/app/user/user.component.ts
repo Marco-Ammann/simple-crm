5a95304bb765f03768a220a33701f4873aa5e081
Marco Ammann <46301477+Melendros@users.noreply.github.com>
2024-05-08 01:34:21 +0200

    added button to add a new user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule, TooltipPosition } from '@angular/material/tooltip';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatDividerModule, MatButtonModule, MatTooltipModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
-
+  positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
+  position = new FormControl(this.positionOptions[1]);
 }
