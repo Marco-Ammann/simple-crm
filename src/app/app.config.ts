bf30879f4ae829944ec941fe304231c79cc22c65
Marco Ammann <46301477+Melendros@users.noreply.github.com>
2024-06-27 00:49:08 +0200

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -9,5 +9,19 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-bd869","appId":"1:503869370378:web:cddd96e2e0cb15db99aff3","storageBucket":"simple-crm-bd869.appspot.com","apiKey":"AIzaSyDvlBMMyU2jDplyg-N0UpPKFVuJCkMvZEo","authDomain":"simple-crm-bd869.firebaseapp.com","messagingSenderId":"503869370378"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase()))]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "simple-crm-bd869",
+      appId: "1:503869370378:web:cddd96e2e0cb15db99aff3",
+      storageBucket: "simple-crm-bd869.appspot.com",
+      apiKey: "AIzaSyDvlBMMyU2jDplyg-N0UpPKFVuJCkMvZEo",
+      authDomain: "simple-crm-bd869.firebaseapp.com",
+      messagingSenderId: "503869370378"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
+  ]
 };
